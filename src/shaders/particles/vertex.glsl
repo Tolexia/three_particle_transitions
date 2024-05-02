2ac0275d17d015577e30bd7ec17bca0ed9985602
Axel C <69471505+Tolexia@users.noreply.github.com>
2024-05-02 19:58:23 +0200

    word Particles transition, text not centered

---
+++ src/shaders/particles/vertex.glsl
@@ -0,0 +1,40 @@
+attribute vec3 aPositionTarget;
+
+uniform vec2 uResolution;
+uniform float uSize;
+uniform float uProgress;
+uniform vec3 uColorA;
+uniform vec3 uColorB;
+
+varying vec3 vColor;
+
+#include ../includes/simplexNoise3d.glsl
+
+void main()
+{
+    // Mixed position
+    float noiseOrigin = simplexNoise3d(position  );
+    float noiseTarget = simplexNoise3d(aPositionTarget  );
+    float noise = mix(noiseOrigin, noiseTarget, uProgress);
+    noise = smoothstep(-1.0, 1.0, noise);
+
+
+    float duration = 0.6;
+    float delay = (1.0 - duration) * noise;
+    float end = delay + duration;
+    float progress = smoothstep(delay, end, uProgress);
+
+    vec3 mixedPosition = mix(position, aPositionTarget, progress);
+
+    // Final position
+    vec4 modelPosition = modelMatrix * vec4(mixedPosition, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    // Point size
+    gl_PointSize = uSize * uSize * uResolution.y;
+    gl_PointSize *= (1.0 / - viewPosition.z);
+
+    vColor = mix(uColorA, uColorB, noise);
+}
